319ff758741d3e63e2440fa38a3fbc9a9bb04f42
Alexey Bulbenko <abulbenko@slice.com>
2020-01-15 23:06:05 +0200

    Refactor. Used format String. Created static variables for db queries.

--- src/main/java/dbExample/StudentsDBSearch.java
+++ src/main/java/dbExample/StudentsDBSearch.java
@@ -7,11 +7,18 @@ public class StudentsDBSearch {
     // shemaDB can be found in resources folder
     private static final String dbUrl = "jdbc:mysql://localhost:3306/testDB";
     private static final String user = "root";
+    private static final String query1 = "select id, fio from students";
+    private static final String query2 = "select id, group_name from groups";
+    private static final String query3 = "select fio, group_name from students join groups on group_id = groups.id where group_id = ?";
+    private static final String query4 = "select fio, year(enter_date) from students order by enter_date";
+    private static final String query5 = "select students.fio as student, class_name, grade, teachers.fio as teacher from students join grades on students.id = grades.student_id join classes on class_id = classes.id join teachers on teacher_id = teachers.id order by students.fio";
+    private static final String query6 = "select fio, AVG(grade) from students join grades on students.id = grades.student_id group by fio";
 
     public static void main(String[] args) throws SQLException, InterruptedException {
         Connection connection = DriverManager.getConnection(dbUrl,user,"");
         System.out.println("SQL Connection to database established!");
         Statement mySt = connection.createStatement();
+
         Scanner scanner = new Scanner(System.in);
         System.err.println("Please Input:" +
                 " 1 - Get All Students" +
@@ -44,61 +51,90 @@ public class StudentsDBSearch {
     }
 
     private static void getAllStudentList(Statement mySt) throws SQLException {
-        ResultSet myRs = mySt.executeQuery("select fio from students");
+        ResultSet myRs = mySt.executeQuery(query1);
         System.out.println("-----ALL STUDENT RESULTS-----");
+
         while (myRs.next()){
-            System.out.print("Student : " + myRs.getString("fio"));
-            System.out.println();
+            System.out.print(myRs.getInt(1));
+            System.out.print(": ");
+            String row = String.format("%s", myRs.getString(2));
+            System.out.println(row);
         }
     }
 
     private static void getStudentListByGroup(Statement mySt, Connection connection) throws SQLException, InterruptedException {
         System.out.println("Our Student Groups are following :");
-        ResultSet myRss = mySt.executeQuery("select id, group_name from groups");
+        ResultSet myRss = mySt.executeQuery(query2);
+
         while (myRss.next()){
-            System.out.print("Group id : " + myRss.getInt("id") + " || Group name : " + myRss.getString("group_name"));
-            System.out.println();
+            System.out.print(myRss.getInt(1));
+            System.out.print(": ");
+            String row = String.format("%s", myRss.getString(2));
+            System.out.println(row);
         }
+
         Scanner sc = new Scanner(System.in);
         Thread.sleep(1000);
+
         System.err.println("Students from which group you want to get results? Input group id you are interested in ");
         int groupId = sc.nextInt();
 
-        PreparedStatement myPs = connection.prepareStatement("select fio, group_name from students join groups on group_id = groups.id where group_id = ?");
+        PreparedStatement myPs = connection.prepareStatement(query3);
         myPs.setInt(1, groupId);
         ResultSet myRs = myPs.executeQuery();
+
         while (myRs.next()){
-            System.out.println(myRs.getString("fio"));
+            String row = String.format("%s", myRs.getString(1));
+            System.out.println(row);
         }
     }
 
     private static void getStudentListByEnterYear(Statement mySt) throws SQLException {
-        ResultSet myRs = mySt.executeQuery("select fio, year(enter_date) from students order by enter_date");
+        ResultSet myRs = mySt.executeQuery(query4);
         System.out.println("-----STUDENTS BY ENTER YEAR RESULTS-----");
+        // used metadata to get column names
+        ResultSetMetaData meta = myRs.getMetaData();
+        String header = String.format("%-21s%s", meta.getColumnName(1), meta.getColumnName(2));
+        System.out.println(header);
+
         while (myRs.next()){
-            System.out.print("Student : " + myRs.getString("fio") + " || ENTER YEAR : " + myRs.getString("year(enter_date)"));
-            System.out.println();
+            String row = String.format("%-21s", myRs.getString(1));
+            System.out.print(row);
+            System.out.println(myRs.getString(2));
         }
     }
 
     private static void getStudentClassesAndGradesWithTeachers(Statement mySt) throws SQLException {
-        ResultSet myRs = mySt.executeQuery("select students.fio as student, class_name, grade, teachers.fio as teacher from students join grades on students.id = grades.student_id join classes on class_id = classes.id join teachers on teacher_id = teachers.id order by students.fio");
+        ResultSet myRs = mySt.executeQuery(query5);
         System.out.println("-----STUDENTS, CLASSES, GRADES AND TEACHERS RESULTS-----");
+
+        ResultSetMetaData meta = myRs.getMetaData();
+        String header = String.format("%-20s%-35s%-12s%s",
+                meta.getColumnName(1),
+                meta.getColumnName(2),
+                meta.getColumnName(3),
+                meta.getColumnName(4));
+        System.out.println(header);
+
         while (myRs.next()){
-            System.out.print("Student : " + myRs.getString("student")
-                    + " || Class : " + myRs.getString("class_name")
-                    + " || Grade : " + myRs.getInt("grade")
-                    + " || Teacher : " + myRs.getString("teacher") );
-            System.out.println();
+            System.out.print(String.format("%-20s", myRs.getString(1)));
+            System.out.print(String.format("%-35s", myRs.getString(2)));
+            System.out.print(String.format("%-12s", myRs.getString(3)));
+            System.out.println(myRs.getString(4));
         }
     }
 
     private static void getStudentsAndTheirAverageGrades(Statement mySt) throws SQLException {
-        ResultSet myRs = mySt.executeQuery("select fio, AVG(grade) from students join grades on students.id = grades.student_id group by fio");
+        ResultSet myRs = mySt.executeQuery(query6);
         System.out.println("-----ALL STUDENT RESULTS-----");
+
+        ResultSetMetaData meta = myRs.getMetaData();
+        String header = String.format("%-21s%s", meta.getColumnName(1), meta.getColumnName(2));
+        System.out.println(header);
+
         while (myRs.next()){
-            System.out.print("Student : " + myRs.getString("fio") + " || Average : " + myRs.getString("AVG(grade)"));
-            System.out.println();
+            System.out.print(String.format("%-21s", myRs.getString(1)));
+            System.out.println(myRs.getString(2));
         }
     }
 }
